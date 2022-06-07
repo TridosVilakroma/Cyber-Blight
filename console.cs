d7a9ddd0ff21af56f450444c833a1c5ff5642d0a
CalebStock <calebstock91@gmail.com>
2022-06-06 22:37:38 -0500

    settings menu updated; can save game from settings

--- console.cs
+++ console.cs
@@ -2,6 +2,8 @@ namespace CyberBlight.console
 {
     using System.Collections.Generic;
     using CyberBlight.engine;
+    using CyberBlight.character_sheet;
+    using CyberBlight.data_IO;
 
 
     public class Terminal
@@ -70,6 +72,41 @@ namespace CyberBlight.console
         
         public static void settings()
         {
+            engine.clear_console();
+            string action = engine.drop_down("Settings", new Dictionary<string, string>
+            {
+                {
+                    "Profile",
+                    "console_profile"},
+                {
+                    "Text Speed",
+                    "console_text_speed"},
+                {
+                    "System Colors",
+                    "console_color"},
+                {
+                    "Save Game",
+                    "console_save"}},">>");
+            if (action == "console_save")
+            {
+                DataIO.save(Player.name, Player.name);
+                action="console_settings";
+            }
+            Logic.focus=action;
+        }
+        public static void profile()
+        {
+            engine.clear_console();
+            string action = engine.drop_down("Console", new Dictionary<string, string>
+            {
+                {
+                    "User Name",
+                    "console_user_name"},
+                {
+                    "Close",
+                    "console_settings"}
+                });
+            Logic.focus=action;
         }
     }
 }
